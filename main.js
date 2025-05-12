bbe600a64b849bccc125616e9a8f68687cd6305a
Erremme <Erremme2.0@gmail.com>
2025-05-12 14:53:15 +0200

    feat: make first bonus

--- main.js
+++ main.js
@@ -4,19 +4,29 @@ Estrarre la proprietà userId dalla ricetta
 Usare userId per ottenere le informazioni dello chef da https://dummyjson.com/users/{userId}
 Restituire la data di nascita dello chef*/
 
+/*Attualmente, se la prima richiesta non trova una ricetta, la seconda richiesta potrebbe comunque essere eseguita causando errori a cascata.
+Modifica getChefBirthday(id) per intercettare eventuali errori prima di fare la seconda richiesta. */
+
 
 async function getRecipe(url) {
   const response = await fetch(url);
+
   const data = await response.json();
   return data;
 }
 
 
 async function getChefBirthday(id) {
-    
-  const ricetta = await getRecipe(`https://dummyjson.com/recipes/${id}`);
-  
 
+    const ricetta = await getRecipe(`https://dummyjson.com/recipes/${id}`);
+
+    if (!ricetta || !ricetta.userId) {
+        throw new Error("Ricetta non trovata ");
+    }
+
+  
+ 
+ 
   const chef = await getRecipe(`https://dummyjson.com/users/${ricetta.userId}`);
   return chef.birthDate;
   
